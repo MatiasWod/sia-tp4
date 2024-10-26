fe94ad3ae1768115c38b3b382dde67ce6306c938
Gaston Alasia <galasia@itba.edu.ar>
2024-10-26 14:29:03 -0300

    feat: alphabet

--- src/runHopfield.py
+++ src/runHopfield.py
@@ -1,31 +1,35 @@
 import numpy as np
 from src.Hopfield import HopfieldNetwork
 
-letters = [np.array([[ 1,  1,  1,  1, 1],
-              [ 1, -1, -1, -1,  1],
-              [ 1,  1,  1,  1, -1],
-              [ 1, -1, -1, -1,  1],
-              [ 1,  1,  1,  1, 1]]),
-           np.array([[1, 1, 1, 1, -1],
-                     [1, -1, -1, 1, -1],
-                     [1, -1, -1, 1, -1],
-                     [1, 1, 1, 1, -1],
-                     [-1, -1, -1, -1, 1]]),
-           np.array([[1, 1, 1, 1, 1],
-                     [-1, -1, 1, -1, -1],
-                     [-1, -1, 1, -1, -1],
-                     [-1, -1, 1, -1, -1],
-                     [-1, -1, 1, -1, -1]]),
-           np.array([[1, -1, -1, -1, 1],
-                     [1, -1, -1, -1, 1],
-                     [-1, 1, -1, 1, -1],
-                     [-1, 1, -1, 1, -1],
-                     [-1, -1, 1, -1, -1]])
-]
+def read_matrices_from_file(filename):
+    matrices = []
+    current_matrix = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                row = [int(num) for num in line.split(',')]
+                current_matrix.append(row)
+
+                if len(current_matrix) == 5:
+                    matrices.append(np.array(current_matrix))
+                    current_matrix = []
+
+    return matrices
+
+# Usage
+filename = './raw_data/alphabet.txt'
+matrices = read_matrices_from_file(filename)
+
+# Example: Print each matrix
+for i, matrix in enumerate(matrices):
+    print(f"Letter {chr(65 + i)}:\n{matrix}\n")
 
 hopfield = HopfieldNetwork()
-hopfield.train(letters)
-original_matrix = letters[3]
+hopfield.train(matrices[3:8])
+print(f"longitud del alphabet: {len(matrices[3:7])}")
+original_matrix = matrices[3]
 noisy_matrix = hopfield.add_noise(original_matrix,0.1)
 hopfield_matrix = hopfield.update(noisy_matrix)
 print("Should print V")
