754dc99a68caf2b118345810f74fbfef5c62a47f
Gaston Alasia <galasia@itba.edu.ar>
2024-10-29 13:42:58 -0300

    feat: kohonen config file

--- src/runKohonen.py
+++ src/runKohonen.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import json
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from src.Models.Kohonen import Kohonen
@@ -16,54 +17,62 @@ standardized_features = scaler.fit_transform(features)
 
 # Inicializar y entrenar el modelo
 input_dim = standardized_features.shape[1]
-map_size = (3, 3)
 
-som = Kohonen(
-    input_dim=input_dim, 
-    map_size=map_size, 
-    learning_rate=0.1, 
-    sigma=3.0, 
-    random_seed=41
-)
-som.train(standardized_features, epochs=500*input_dim)
+with open('./configs/kohonen_config.json', 'r') as f:
+    config = json.load(f)
 
-# 5. Obtener las posiciones BMU para cada país
-bmu_coordinates = som.transform(standardized_features)
-data['BMU'] = [tuple(coord) for coord in bmu_coordinates]
+    k = config["k"]
+    learning_rate = config["learning_rate"]
+    sigma = config["sigma"]
 
-# Crear un diccionario para almacenar los países en cada celda porque se pueden superponer
-bmu_dict = {}
-for _, row in data.iterrows():
-    bmu = row['BMU']
-    country = row['Country']
-    if bmu not in bmu_dict:
-        bmu_dict[bmu] = []
-    bmu_dict[bmu].append(country)
+    map_size = (k, k)
 
-# Gráfico del Mapa con los países enlistados en cada coordenada
-fig, ax = plt.subplots(figsize=(8, 8))
+    som = Kohonen(
+        input_dim=input_dim, 
+        map_size=map_size, 
+        learning_rate=learning_rate, 
+        sigma=sigma, 
+        random_seed=41
+    )
+    som.train(standardized_features, epochs=500*input_dim)
 
-# Graficar el mapa
-for i in range(map_size[0]):
-    for j in range(map_size[1]):
-        # Obtener los países de la coordenada actual
-        countries_in_cell = bmu_dict.get((i, j), [])
-        text = "\n".join(countries_in_cell)  # Unir los nombres con saltos de línea
-        ax.text(j, i, text, ha='center', va='center', fontsize=8)
+    # 5. Obtener las posiciones BMU para cada país
+    bmu_coordinates = som.transform(standardized_features)
+    data['BMU'] = [tuple(coord) for coord in bmu_coordinates]
 
-# Configurar límites y estética del gráfico
-ax.set_xlim(-0.5, map_size[1] - 0.5)  # Limites x
-ax.set_ylim(-0.5, map_size[0] - 0.5)  # Limites y
+    # Crear un diccionario para almacenar los países en cada celda porque se pueden superponer
+    bmu_dict = {}
+    for _, row in data.iterrows():
+        bmu = row['BMU']
+        country = row['Country']
+        if bmu not in bmu_dict:
+            bmu_dict[bmu] = []
+        bmu_dict[bmu].append(country)
 
-ax.set_xticks(np.arange(map_size[1]))  # Marcas en x desde 0 hasta map_size[1]-1
-ax.set_yticks(np.arange(map_size[0]))  # Marcas en y desde 0 hasta map_size[0]-1
-ax.set_xticklabels(np.arange(map_size[1]))  # Etiquetas en x
-ax.set_yticklabels(np.arange(map_size[0]))  # Etiquetas en y
-ax.grid(which='minor', color='black', linestyle='-', linewidth=1)
-ax.invert_yaxis()  # Invertir el eje Y para alinear correctamente
+    # Gráfico del Mapa con los países enlistados en cada coordenada
+    fig, ax = plt.subplots(figsize=(8, 8))
 
-plt.title('SOM - Mapa de Países Agrupados')
-plt.show()
+    # Graficar el mapa
+    for i in range(map_size[0]):
+        for j in range(map_size[1]):
+            # Obtener los países de la coordenada actual
+            countries_in_cell = bmu_dict.get((i, j), [])
+            text = "\n".join(countries_in_cell)  # Unir los nombres con saltos de línea
+            ax.text(j, i, text, ha='center', va='center', fontsize=8)
+
+    # Configurar límites y estética del gráfico
+    ax.set_xlim(-0.5, map_size[1] - 0.5)  # Limites x
+    ax.set_ylim(-0.5, map_size[0] - 0.5)  # Limites y
+
+    ax.set_xticks(np.arange(map_size[1]))  # Marcas en x desde 0 hasta map_size[1]-1
+    ax.set_yticks(np.arange(map_size[0]))  # Marcas en y desde 0 hasta map_size[0]-1
+    ax.set_xticklabels(np.arange(map_size[1]))  # Etiquetas en x
+    ax.set_yticklabels(np.arange(map_size[0]))  # Etiquetas en y
+    ax.grid(which='minor', color='black', linestyle='-', linewidth=1)
+    ax.invert_yaxis()  # Invertir el eje Y para alinear correctamente
+
+    plt.title('SOM - Mapa de Países Agrupados')
+    plt.show()
 
 # Gráfico de la matriz de distancias promedio entre neuronas vecinas
 def calculate_u_matrix(som):
