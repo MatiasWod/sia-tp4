fa33fb17ce672f25986939b79cf794a62faa45e6
Juan Segundo Arnaude <juansarnaude@gmail.com>
2024-10-28 21:43:32 -0300

    fixed oja

--- src/runOja.py
+++ src/runOja.py
@@ -11,8 +11,8 @@ normalized_data = scaler.fit_transform(data[numerical_cols])
 normalized_df = pd.DataFrame(normalized_data, columns=numerical_cols)
 
 input_data_len = normalized_data.shape[1]
-iterations = 100
-learning_rate = 0.01
+iterations = 10000
+learning_rate = 0.1
 random_seed = 42
 
 oja_model = Oja(normalized_data, input_data_len, learning_rate, random_seed=random_seed)
@@ -27,10 +27,18 @@ print("Final weights:", final_weights)
 pca_evals = oja_model.evaluate(normalized_data)
 
 countries = data.iloc[:, 0]
-plt.figure(figsize=(12, 6))
-plt.bar(countries, pca_evals)
-plt.xticks(rotation=90)
-plt.xlabel('Country')
-plt.ylabel('PCA 1')
-plt.title('PCA 1 Analysis for Countries with Oja')
+plt.figure(figsize=(12, 8))
+bars = plt.bar(countries, pca_evals)
+plt.xticks(rotation=90, fontsize=20)
+plt.xlabel('Country', fontsize=20)
+plt.ylabel('PC 1', fontsize=20)
+plt.title('PC 1 Analysis for Countries with Oja', fontsize=20)
+plt.tight_layout()
+
+for bar in bars:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2, yval, round(yval, 2),
+             ha='center', va='bottom', fontsize=12, color="black")
+
 plt.show()
+
